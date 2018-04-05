17921e574edda343b1615622546a6c6608c501ea
Max S <m123@example.com>
2018-04-05 23:43:23 +0200

    Working version.

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,7 +5,6 @@ public class MyClass {
     public static void main(String[] args) {
         String dupa = "123";
         System.out.println("My text: " + dupa);
-        System.out.println("new branch  
     }
 
 
